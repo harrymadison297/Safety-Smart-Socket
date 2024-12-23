91e7bbc675ddb88562aec00b3042bb3fb93a70a4
harrymadison297 <harrymadison297@gmail.com>
2024-12-24 00:31:54 +0700

    authHear

--- SW_Safety_Socket/Cloud/src/routes/user/index.js
+++ SW_Safety_Socket/Cloud/src/routes/user/index.js
@@ -1,9 +1,13 @@
 import express from "express"
 import userAuthRouter from "./auth.routes.js"
+import asyncMiddlewareHandler from "../../services/middleware/middleware.service.js"
+import { AuthenticationHeader } from "../../services/middleware/header.service.js"
 
 const userRouter = express.Router()
 
 userRouter
     .use('/auth', userAuthRouter)
+    .use(asyncMiddlewareHandler(AuthenticationHeader))
+    
  
 export default userRouter
--- SW_Safety_Socket/Cloud/src/services/keyToken.service.js
+++ SW_Safety_Socket/Cloud/src/services/keyToken.service.js
@@ -32,4 +32,6 @@ const signTokenID = async (userID) => {
   });
 };
 
+
+
 export { signTokenID, updateAccessToken };
