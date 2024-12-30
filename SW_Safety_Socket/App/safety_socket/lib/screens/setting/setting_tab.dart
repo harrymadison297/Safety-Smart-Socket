d13372e253b819f103997a90d5f8a3895297aff4
harrymadison297 <harrymadison297@gmail.com>
2024-12-31 02:48:04 +0700

    SettingPage_Logout

--- SW_Safety_Socket/App/safety_socket/lib/screens/setting/setting_tab.dart
+++ SW_Safety_Socket/App/safety_socket/lib/screens/setting/setting_tab.dart
@@ -1,10 +1,162 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:iconly/iconly.dart';
+import 'package:provider/provider.dart';
+import 'package:safety_socket/bloc/auth/auth_bloc.dart';
+
+import '../../config/router.dart';
+import '../../datas/models/user_model.dart';
 
 class SettingTab extends StatelessWidget {
   const SettingTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Consumer<UserModel>(
+        builder: (context, value, child) => Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 60, horizontal: 30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                "Settings",
+                style: TextStyle(
+                  fontSize: 36,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 40),
+              const Text(
+                "Account",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              const SizedBox(height: 20),
+              SizedBox(
+                width: double.infinity,
+                child: Row(
+                  children: [
+                    Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.orange.shade100,
+                      ),
+                      child: const Icon(
+                        IconlyLight.heart,
+                        color: Colors.orange,
+                      ),
+                    ),
+                    const SizedBox(width: 20),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          value.name,
+                          style: const TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        Text(
+                          value.email,
+                          style: const TextStyle(
+                            fontSize: 14,
+                            color: Colors.grey,
+                          ),
+                        )
+                      ],
+                    ),
+                    const Spacer(),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 40),
+              const Text(
+                "Settings",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              const SizedBox(height: 20),
+              SettingItem(
+                title: "Language",
+                icon: IconlyLight.bag,
+                bgColor: Colors.blue.shade100,
+                iconColor: Colors.blue,
+                value: "English",
+                onTap: () {},
+              ),
+              const SizedBox(height: 20),
+              SettingItem(
+                title: "Log out",
+                icon: IconlyLight.logout,
+                bgColor: Colors.red.shade100,
+                iconColor: Colors.red,
+                onTap: () {
+                  context.read<AuthBloc>().add(AuthLogoutStarted());
+                  context.go(RouteName.login);
+                },
+              ),
+              const SizedBox(height: 20),
+            ],
+          ),
+        ),
+      ),
+    ));
+  }
+
+  Widget SettingItem ({required String title,required Color bgColor,required Color iconColor,required IconData icon,required Function() onTap, String? value}) {
+    return TextButton(
+        onPressed: onTap,
+        style: TextButton.styleFrom(
+            padding: const EdgeInsets.fromLTRB(0, 0, 0, 0)
+        ),
+        child: SizedBox(
+      width: double.infinity,
+      child: Row(
+        children: [
+          Container(
+            height: 50,
+            width: 50,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: bgColor,
+            ),
+            child: Icon(
+              icon,
+              color: iconColor,
+            ),
+          ),
+          const SizedBox(width: 20),
+          Text(
+            title,
+            style: const TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w500,
+              color: Colors.black87
+            ),
+          ),
+          const Spacer(),
+          value != null
+              ? Text(
+            value,
+            style: const TextStyle(
+              fontSize: 16,
+              color: Colors.grey,
+            ),
+          )
+              : const SizedBox(),
+          const SizedBox(width: 20),
+        ],
+      ),
+    ));
   }
 }
